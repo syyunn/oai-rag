0a30ab82626e64f5af1f2ba7a54fbb0c249e6f98
Suyeol Yun <syyun@mit.edu>
2024-01-04 08:52:38 -0600

    add

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ from streamlit.logger import get_logger
 from meta import meta
 from main import main   
 
-LOGGER = get_logger(__name__)
-
-use_auth = False
-
 def app():
     meta()  # Set meta-information for the app
     main()
